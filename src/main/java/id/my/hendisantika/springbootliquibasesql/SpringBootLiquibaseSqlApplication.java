8f5293cad122f88ee1fb3162ea22b9cd20b1d54a
hendisantika <hendisantika@yahoo.co.id>
2024-05-15 08:53:53 +0700

    Create Spring Boot project 🫸🌀✏️📗🐧🐳

---
+++ src/main/java/id/my/hendisantika/springbootliquibasesql/SpringBootLiquibaseSqlApplication.java
@@ -0,0 +1,13 @@
+package id.my.hendisantika.springbootliquibasesql;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootLiquibaseSqlApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootLiquibaseSqlApplication.class, args);
+    }
+
+}
---
+++ src/test/java/id/my/hendisantika/springbootliquibasesql/SpringBootLiquibaseSqlApplicationTests.java
@@ -0,0 +1,13 @@
+package id.my.hendisantika.springbootliquibasesql;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootLiquibaseSqlApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
